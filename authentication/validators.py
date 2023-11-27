27f0c881e6215e8e5477a7c25564bd655fd44dc0
Aliful Islam Shuvo <alif.seu@gmail.com>
2023-11-27 13:16:49 +0600

    Update validator for gmail.

--- authentication/validators.py
+++ authentication/validators.py
@@ -3,14 +3,14 @@ import re
 from rest_framework.exceptions import ValidationError
 
 
-def validate_aamarpay_email(email):
+def validate_email(email):
     """
-    Validate if email is an STIL email.
+    Validate if email is an gmail.
     """
-    email_regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@aamarpay.com')
+    email_regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@gmail.com')
     if re.fullmatch(email_regex, email):
         return email
-    raise ValidationError("Please Provide a valid STIL email.")
+    raise ValidationError("Please Provide a valid gmail.")
 
 
 
